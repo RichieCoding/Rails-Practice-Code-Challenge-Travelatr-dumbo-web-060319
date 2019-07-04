b7b51736918e02a2fc7ca5409c70e7e1bcb9adc8
Richard Li <richardli125@gmail.com>
2019-07-04 15:24:33 -0400

    Finished Bloggers and Destinations

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,13 @@
 class Blogger < ApplicationRecord
+  has_many :posts
+  has_many :destinations, through: :posts
+
+  def most_popular
+    self.posts.sort_by { |post| post.title }[0]
+  end
+
+  def top_five_destination
+    self.destinations.sort_by { |destination| destination.posts.count}[0..4] 
+  end
 
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,14 @@
 class Destination < ApplicationRecord
+  has_many :posts
+  has_many :bloggers, through: :posts
+
+
+  def five_most_post
+    self.posts.sort_by {|post| post.updated_at}[0..4]
+  end
+
+  def most_popular
+    self.posts.sort_by { |post| post.likes }[0]
+  end
 
 end
